52418ad78427cc629accfc0d2306dd6541b54c1e
herbentg <herbentg@hermeszsoft.hu>
2023-08-09 15:54:33 +0200

    remove consol

--- src/hooks/useMoveItemFunction.js
+++ src/hooks/useMoveItemFunction.js
@@ -1,9 +1,5 @@
 export const useMoveItemFunction = ({ character, setCharacter }) => {
   const moveItemFunction = (fromId, what, toId) => {
-    console.log(fromId);
-    console.log(what);
-    console.log(toId);
-
     const inventoryMap = character?.inventory?.map((inv) => {
       if (inv?.id === fromId) return { ...inv, item: null };
       if (inv?.id === toId) return { ...inv, item: what };
@@ -15,10 +11,6 @@ export const useMoveItemFunction = ({ character, setCharacter }) => {
       if (slot?.id === toId) return { ...slot, item: what };
       return slot;
     });
-
-    console.log(character);
-    console.log(inventoryMap);
-    console.log(equipmentMap);
 
     setCharacter({
       ...character,
